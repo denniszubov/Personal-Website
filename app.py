4211278c1ccd3eb325d7554137a2344f8bed8a8a
dennis.zubov <denniszubov365@gmail.com>
2022-10-22 19:05:51 -0400

    Add age calculation and basic information

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
 from flask import Flask, jsonify, abort, request, make_response, url_for, session
 from flask import render_template, redirect
-
+from datetime import date
 
 app = Flask(__name__)
 
+def calculate_age(birthdate):
+    # Calculate Age
+    today = date.today()
+    birthdate = date(year=2001, month=11, day=26)
+    age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+    return age
 
 @app.route('/', methods=['GET'])
 def home_page():
-    return '<h1>Hello, World!</h1>'
+    data = {}
+    age = calculate_age(date(year=2001, month=11, day=26))
+    email = "***REMOVED***"
+    based = "Atlanta, GA"
+    languages = "English, French, Russian"
+
+    data["age"] = age
+    data["email"] = email
+    data["based"] = based
+    data["languages"] = languages
+
+    return render_template("index.html", data=data)
 
 
 if __name__ == '__main__':
