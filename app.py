aa04f056ef236df4ecae60174390bfadf155d425
Dennis Zubov <denniszubov365@gmail.com>
2024-03-01 11:33:10 -0500

    Imporve security in application and refactor

--- app.py
+++ app.py
@@ -1,39 +1,22 @@
-from flask import Flask, request, url_for, render_template, redirect, send_file, request
+from flask import Flask, request, render_template, send_file, request
 from flask_mail import Mail, Message
-from datetime import date
-from env import DATA
+from helpers import getDynamicData
+from dotenv import load_dotenv
+import os
 
+
+load_dotenv()
 
 app = Flask(__name__)
 mail = Mail(app)
 
-app.config["MAIL_SERVER"] = "***REMOVED***"
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = "***REMOVED***"
-app.config['MAIL_PASSWORD'] = '***REMOVED***'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
+app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT"))
+app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER")
+app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+app.config["MAIL_USE_TLS"] = os.getenv("MAIL_USE_TLS").lower() in ("true", "1", "t")
+app.config["MAIL_USE_SSL"] = os.getenv("MAIL_USE_SSL").lower() in ("true", "1", "t")
 mail = Mail(app)
-
-
-def calculate_age(birthdate):
-    # Calculate Age
-    today = date.today()
-    birthdate = date(year=2001, month=11, day=26)
-    age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
-    return age
-
-
-def getDynamicData(DATA):
-    age = calculate_age(date(year=2001, month=11, day=26))
-    year = date.today().year
-
-    data = DATA
-
-    data["age"] = age
-    data["year"] = year
-
-    return data
 
 
 def send_email(request):
@@ -51,7 +34,7 @@ def send_email(request):
 
 @app.route('/', methods=['GET', 'POST'])
 def home_page():
-    data = getDynamicData(DATA)
+    data = getDynamicData()
     if request.method == "POST":
         send_email(request.form)
     return render_template("index.html", data=data)
