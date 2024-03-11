fcdfbc73f8822b07377b7e406a12d5ded8661038
Dennis Zubov <denniszubov365@gmail.com>
2024-03-10 20:28:16 -0400

    Add certifications

--- data.py
+++ data.py
@@ -21,6 +21,9 @@ links = {
     "georgia_tech": "https://www.linkedin.com/school/georgia-institute-of-technology/",
     "pimco": "https://www.linkedin.com/company/pimco",
     "jurumani_solutions": "https://www.linkedin.com/company/jurumani/",
+    "bmc_certification": "https://portal.bloombergforeducation.com/certificates/EAguZiFXcX6e1y2aaUSeAANG",
+    "supervised_ml": "https://www.coursera.org/account/accomplishments/certificate/SVGVFVB443MZ",
+    "advanced_learning_algos": "https://www.coursera.org/account/accomplishments/certificate/5UQ44RKC4JQV",
 }
 
 DATA["skills"] = skills
