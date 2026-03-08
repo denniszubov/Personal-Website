ee1ec90865f73f4c51cdcda75e28228fe77f8d63
Dennis Zubov <denniszubov365@gmail.com>
2026-03-08 15:52:01 -0400

    separate information from html

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,23 +1,192 @@
-// Add your javascript here
-// Don't forget to add it into respective layouts where this js file is needed
-
+// Fetch data.json and render all sections
 $(document).ready(function() {
-  AOS.init( {
-    // uncomment below for on-scroll animations to played only once
-    // once: true
-  }); // initialize animate on scroll library
-
   // Set current year in footer
   document.getElementById('year').textContent = new Date().getFullYear();
+
+  fetch('data.json')
+    .then(function(res) { return res.json(); })
+    .then(function(data) {
+      renderHero(data);
+      renderAbout(data.about);
+      renderEducation(data.education);
+      renderSkills(data.skills);
+      renderExperience(data.experience);
+      renderCertifications(data.certifications);
+      renderProjects(data.projects);
+      renderFooter(data.links);
+
+      // Re-initialize AOS after content is injected
+      AOS.init();
+    });
 });
+
+function renderHero(data) {
+  document.getElementById('hero-name').textContent = data.name;
+  document.getElementById('hero-tagline').textContent = data.tagline;
+
+  document.getElementById('hero-linkedin').innerHTML =
+    '<a class="btn btn-primary btn-contact" href="' + data.links.linkedin + '" target="_blank" data-aos="zoom-in" data-aos-anchor="data-aos-anchor">Message on LinkedIn</a>';
+
+  document.getElementById('social-buttons').innerHTML =
+    '<a class="btn btn-default btn-round btn-lg btn-icon" href="' + data.links.linkedin + '" rel="tooltip" title="Follow me on Linkedin" target="_blank"><i class="fa fa-linkedin"></i></a>' +
+    '<a class="btn btn-default btn-round btn-lg btn-icon" href="' + data.links.github + '" rel="tooltip" title="Follow me on GitHub" target="_blank"><i class="fa fa-github"></i></a>';
+}
+
+function renderAbout(about) {
+  var aboutHtml = '';
+  about.paragraphs.forEach(function(p) {
+    aboutHtml += '<p>' + p + '</p>';
+  });
+  document.getElementById('about-text').innerHTML = aboutHtml;
+
+  var infoHtml = '';
+  var keys = Object.keys(about.info);
+  keys.forEach(function(key, i) {
+    var mt = i > 0 ? ' mt-3' : '';
+    infoHtml +=
+      '<div class="row' + mt + '">' +
+        '<div class="col-sm-4"><strong class="text-uppercase">' + key + ':</strong></div>' +
+        '<div class="col-sm-8">' + about.info[key] + '</div>' +
+      '</div>';
+  });
+  document.getElementById('basic-info').innerHTML = infoHtml;
+}
+
+function renderEducation(education) {
+  var html = '';
+  education.forEach(function(edu) {
+    var detailsHtml = '';
+    edu.details.forEach(function(d) {
+      detailsHtml += '<li>' + d + '</li>';
+    });
+    html +=
+      '<div class="card">' +
+        '<div class="row">' +
+          '<div class="col-md-3" data-aos="fade-right" data-aos-offset="50" data-aos-duration="500">' +
+            '<div class="card-body cc-education-header">' +
+              '<img src="' + edu.logo + '" alt="' + edu.logoAlt + '"/>' +
+            '</div>' +
+          '</div>' +
+          '<div class="col-md-9" data-aos="fade-left" data-aos-offset="50" data-aos-duration="500">' +
+            '<div class="card-body">' +
+              '<div class="h5">' + edu.title + '</div>' +
+              '<p class="category">' + edu.school + '<br>' + edu.dates + '</p>' +
+              '<p><ul>' + detailsHtml + '</ul></p>' +
+            '</div>' +
+          '</div>' +
+        '</div>' +
+      '</div>';
+  });
+  document.getElementById('education-list').innerHTML = html;
+}
+
+function renderSkills(skills) {
+  var keys = Object.keys(skills);
+  var half = Math.ceil(keys.length / 2);
+  var col1 = keys.slice(0, half);
+  var col2 = keys.slice(half);
+
+  function buildCol(arr) {
+    var h = '';
+    arr.forEach(function(key) {
+      h +=
+        '<div class="skill-category">' +
+          '<div class="h6 skill-category-title">' + key + '</div>' +
+          '<p>' + skills[key] + '</p>' +
+        '</div>';
+    });
+    return h;
+  }
+
+  document.getElementById('skills-content').innerHTML =
+    '<div class="col-md-6">' + buildCol(col1) + '</div>' +
+    '<div class="col-md-6">' + buildCol(col2) + '</div>';
+}
+
+function renderExperience(experience) {
+  var html = '';
+  experience.forEach(function(exp) {
+    html +=
+      '<div class="card">' +
+        '<div class="row">' +
+          '<div class="col-md-3" data-aos="fade-right" data-aos-offset="50" data-aos-duration="500">' +
+            '<div class="card-body cc-education-header">' +
+              '<a href="' + exp.link + '" target="_blank"><img src="' + exp.logo + '" alt="' + exp.logoAlt + '"/></a>' +
+            '</div>' +
+          '</div>' +
+          '<div class="col-md-9" data-aos="fade-left" data-aos-offset="50" data-aos-duration="500">' +
+            '<div class="card-body">' +
+              '<div class="h5">' + exp.title + '</div>' +
+              '<p class="category">' + exp.company + '<br>' + exp.location + '<br>' + exp.dates + '</p>' +
+              '<p>' + exp.description + '</p>' +
+            '</div>' +
+          '</div>' +
+        '</div>' +
+      '</div>';
+  });
+  document.getElementById('experience-list').innerHTML = html;
+}
+
+function renderCertifications(certifications) {
+  var html = '';
+  certifications.forEach(function(cert) {
+    html +=
+      '<div class="card">' +
+        '<div class="row">' +
+          '<div class="col-md-3" data-aos="fade-right" data-aos-offset="50" data-aos-duration="500">' +
+            '<div class="card-body cc-education-header">' +
+              '<a href="' + cert.logoLink + '" target="_blank"><img src="' + cert.logo + '" alt="' + cert.logoAlt + '"/></a>' +
+            '</div>' +
+          '</div>' +
+          '<div class="col-md-9" data-aos="fade-left" data-aos-offset="50" data-aos-duration="500">' +
+            '<div class="card-body">' +
+              '<div class="h5">' + cert.title + '</div>' +
+              '<p class="category">' + cert.date + '</p>' +
+              '<p><a class="link-primary" href="' + cert.certLink + '" target="_blank"><b>View certification</b></a></p>' +
+              '<p>' + cert.contents + '</p>' +
+            '</div>' +
+          '</div>' +
+        '</div>' +
+      '</div>';
+  });
+  document.getElementById('certifications-list').innerHTML = html;
+}
+
+function renderProjects(projects) {
+  var html = '';
+  projects.forEach(function(proj, i) {
+    // Alternate columns: even index = left col, odd = right col
+    if (i % 2 === 0) html += '<div class="col-md-6">';
+    html +=
+      '<div class="card" data-aos="fade-up" data-aos-anchor-placement="top-bottom">' +
+        '<div class="card-body">' +
+          '<div class="h5 text-center">' + proj.title + '</div>' +
+          '<div class="cc-porfolio-image img-raised">' +
+            '<figure class="cc-effect">' +
+              '<img src="' + proj.image + '" alt="' + proj.imageAlt + '"/>' +
+              '<figcaption><br><p>' + proj.description + '</p></figcaption>' +
+            '</figure>' +
+          '</div>' +
+        '</div>' +
+      '</div>';
+    if (i % 2 === 1 || i === projects.length - 1) html += '</div>';
+  });
+  document.getElementById('projects-list').innerHTML = html;
+}
+
+function renderFooter(links) {
+  document.getElementById('footer-social').innerHTML =
+    '<a class="cc-linkedin btn btn-link" href="' + links.linkedin + '" target="_blank"><i class="fa fa-linkedin fa-2x " aria-hidden="true"></i></a>' +
+    '<a class="cc-github btn btn-link" href="' + links.github + '" target="_blank"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>';
+}
 
 // Smooth scroll for links with hashes
 $('a.smooth-scroll')
 .click(function(event) {
   // On-page links
   if (
-    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-    && 
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+    &&
     location.hostname == this.hostname
   ) {
     // Figure out element to scroll to
